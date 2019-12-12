8ad08f93379c0d7584beec3545932ae6d0107edb
dezy1ex <dezy1ex@gmail.com>
2019-12-12 22:35:29 +0400

    adding profiles cell

--- alcodo/profiles/models.py
+++ alcodo/profiles/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 
-# Create your models here.
+class UserProfile(models.Model):  
+    user = models.OneToOneField(User, on_delete=models.PROTECT)  
+    city = models.CharField(max_length=64)
+    photo = models.CharField(max_length=64, default="anonymous-user.png")
+
+    def __str__(self):  
+          return "%s's profile" % self.user  
+
+
+def create_user_profile(sender, instance, created, **kwargs):  
+    if created:  
+       profile, created = UserProfile.objects.get_or_create(user=instance)  
+
+
+post_save.connect(create_user_profile, sender=User) 
--- alcodo/profiles/views.py
+++ alcodo/profiles/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import redirect
 
 # from social_django.models import UserSocialAuth
 from django.contrib.auth.models import User
+from profiles.models import UserProfile
 from django.contrib.auth import logout
 
 def mypage(request):
@@ -12,7 +13,8 @@ def mypage(request):
         user = User.objects.get(id=request.session['_auth_user_id'])
     else:
         user = None
-    return render(request, 'base.html', {'user': user})
+    profiles = UserProfile.objects.filter(city="Moscow")
+    return render(request, 'base.html', {'user': user, "profiles": profiles})
 
 def logout_view(request):
     logout(request)
